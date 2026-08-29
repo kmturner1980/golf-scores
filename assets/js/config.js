f27f5f09e87655fde2a702fe01ba4c1698aef8ff
Kevin Turner <kturner@scentsy.com>
2026-08-29 13:50:39 -0600

    Update Config to latest Google Sheets release

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
 // After you deploy the Apps Script project as a Web App, paste the URL it
 // gives you here (looks like https://script.google.com/macros/s/XXXX/exec).
-const API_URL = 'https://script.google.com/macros/s/AKfycbw1MjG9pCp_89twv7eLfel_3Ke1uNCkRXH5cTDeTScsILMT_o8m1_Aqw3pOIgS_M8YdSg/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxC_n_uTluT0-dZaOSTxTP7Mo0YZza0il6ZJL36HCsn9G5hg_SE8tNxpZMr4WWVULUeOw/exec';
